0f80427142b84b40884745c16d4a129fd4d99973
evgeniy-shpyra <foksmix256@gmail.com>
2024-07-16 12:17:55 +0300

    dropout lock

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,15 +1,18 @@
 const browser = mp.browsers.new('package://index.html')
 
-const seatbeltCommandName = 'seatbeltOn'
+const canFlyFlag = 32
 
 function handleSafetyBeltOn() {
   mp.events.callRemote(seatbeltCommandName)
 }
 
 mp.events.add('changeSeatbelt', (isOn) => {
+  const player = mp.players.local
   if (isOn) {
+    player.setConfigFlag(canFlyFlag, false)
     mp.gui.chat.push(`!{00FF00}Ви пристібнули ремінь безпеки`)
   } else {
+    player.setConfigFlag(canFlyFlag, true)
     mp.gui.chat.push(`!{FF0000}Ви відстібнули ремінь безпеки`)
   }
   browser.execute(`toggleSeatbeltIcons(${isOn})`)
